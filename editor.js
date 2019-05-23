a75b97bfede3bffa0a0f524d7c9ae1be51dacaa0
mtsgi <oq@live.jp>
2019-05-23 18:04:47 +0900

    Add Notes Drawing and Change Object Type for Loading

--- editor.js
+++ editor.js
@@ -1,20 +1,26 @@
-var arrobj = new Array();
-var prev;
+var fumenObject = {
+    "raku": [], "easy": [], "normal": [], "hard": [], "extra": []
+};
+var prev = {
+    "raku": null, "easy": null, "normal": null, "hard": null, "extra": null
+};
+var measureHeight = 200;
+var currentLevel = "easy"
 
 $(document).ready( function(){
-    $("#output").html( JSON.stringify(arrobj, null, 4) );
+    $("#output").html( JSON.stringify(fumenObject[currentLevel], null, 4) );
 
     $("#form-add").on("click", ()=>{
         let type = Number($("#form-type").val())
         let measure = Number($("#form-measure").val());
         let lane = {};
         if( type == 1 || type ==2 ){
-            lane = Number($("#form-lane").val());
+            lane = Number($("#form")[0].lane.value);
         }
         let position = Number($("#form-position").val());
         let split = Number($("#form-split").val());
         let option = Number($("#form-option").val());
-        let end = {};
+        let end = new Array();
         if( type == 2 ){
             end = [
                 {
@@ -23,12 +29,12 @@ $(document).ready( function(){
                     "lane": Number($("#endform-lane").val()),
                     "position": Number($("#endform-position").val()),
                     "split": Number($("#endform-split").val()),
-                    option: -1,
-                    end: new Array()
+                    "option": -1,
+                    "end": new Array()
                 }
             ]
         }
-        arrobj.push({
+        fumenObject[currentLevel].push({
             "type": type,
             "measure": measure,
             "lane": lane,
@@ -37,22 +43,40 @@ $(document).ready( function(){
             "option": option,
             "end": end
         });
-        $("#output").html( JSON.stringify(arrobj, null, 4) );
+        $("#output").html( JSON.stringify(fumenObject[currentLevel], null, 4) );
+
+        drawPreview(fumenObject[currentLevel]);
+    });
+
+    $("#form-load").on("click", ()=>{
+        let _level = $("#form-level").val();
+        if( !fumenObject[_level] ){
+            message( _level + "は不明な難易度です");
+        }
+        else{
+            currentLevel = _level;
+            message( _level + "をロードしました");
+            $("#output").html( JSON.stringify(fumenObject[currentLevel], null, 4) );
+            drawPreview(fumenObject[currentLevel]);
+        }
     });
 
     $("#form-undo").on("click", ()=>{
-        prev = arrobj.pop() || prev;
-        $("#output").html( JSON.stringify(arrobj, null, 4) );
+        prev[currentLevel] = fumenObject[currentLevel].pop() || prev[currentLevel];
+        $("#output").html( JSON.stringify(fumenObject[currentLevel], null, 4) );
+        drawPreview(fumenObject[currentLevel]);
     });
 
     $("#form-redo").on("click", ()=>{
-        if( !prev ){
-            $("#debug").text("Redoできません。");
+        if( !prev[currentLevel] ){
+            message("Redoできません");
             return;
         };
-        arrobj.push( prev );
-        prev = null;
-        $("#output").html( JSON.stringify(arrobj, null, 4) );
+        fumenObject[currentLevel].push( prev[currentLevel] );
+        prev[currentLevel] = null;
+        $("#output").html( JSON.stringify(fumenObject[currentLevel], null, 4) );
+        message("Redoしました");
+        drawPreview(fumenObject[currentLevel]);
     });
 
     $("#copy2cb").on("click", function(){
@@ -71,5 +95,22 @@ $(document).ready( function(){
         else $("#endform").hide();
     });
 
-    $("#debug").text("読み込みが完了しました。");
+    message( currentLevel + "をロードしました");
 } );
+
+function drawPreview(obj){
+    $("#preview").html("");
+    let notesnum = 0, maxMeasure = 0;
+    //1ノートずつ処理
+    for( let i of obj ){
+        notesnum++;
+        $("#preview").append("<span id='note"+notesnum+"'></span>");
+        let noteEl = $("#note"+notesnum);
+        noteEl.addClass("type"+i.type).css("right", (Number(i.lane)-1)*60 ).css("top", (Number(i.measure)*measureHeight) + measureHeight*(Number(i.position)/Number(i.split)) );
+    }
+    $("#notesnum").text(notesnum+" notes");
+}
+
+function message( text ){
+    $("#debug").text( text );
+}
